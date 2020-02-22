2344837f4c42ede8ee515086dcbe9d52e826d798
SuupurSanicc <48994999+SuupurSanicc@users.noreply.github.com>
2020-02-21 23:16:17 -0600

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Direction currentDir;
+    Vector2 input;
+    bool isMoving = false;
+    Vector3 startPos;
+    Vector3 endPos;
+    float t;
+
+    public float walkSpeed = 3f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    public bool isAllowedToMove = true;
+
+    void Start()
+    {
+        isAllowedToMove = true;
+    }
+
+    void Update()
+    {
+
+        if (!isMoving && isAllowedToMove)
+        {
+            input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+                input.y = 0;
+            else
+                input.x = 0;
+
+            if (input != Vector2.zero)
+            {
+
+                if (input.x < 0)
+                {
+                    currentDir = Direction.West;
+                }
+                if (input.x > 0)
+                {
+                    currentDir = Direction.East;
+                }
+                if (input.y < 0)
+                {
+                    currentDir = Direction.South;
+                }
+                if (input.y > 0)
+                {
+                    currentDir = Direction.North;
+                }              
+
+                StartCoroutine(Move(transform));
+
+                animator.SetFloat("Horizontal", input.x);
+                animator.SetFloat("Vertical", input.y);
+
+            }
+
+        }
+
+    }
+
+    public IEnumerator Move(Transform entity)
+    {
+        isMoving = true;
+        startPos = entity.position;
+        t = 0;
+
+        endPos = new Vector3(startPos.x + System.Math.Sign(input.x), startPos.y + System.Math.Sign(input.y), startPos.z);
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * walkSpeed;
+            entity.position = Vector3.Lerp(startPos, endPos, t);
+            yield return null;
+        }
+
+        isMoving = false;
+        yield return 0;
+    }
+}
+
+enum Direction
+{
+    North,
+    East,
+    South,
+    West
+}
